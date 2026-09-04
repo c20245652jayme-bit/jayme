3dd800fd31c4bd0284e116133a8e354de2f6354d
c20245652jayme-bit <c20245652.jayme@csav.edu.ph>
2026-09-04 15:26:44 +0800

    functions

--- script.js
+++ script.js
@@ -11,6 +11,15 @@ age = 20;
 console.log (`My name is ${myName}, ${age} yrs old.`);
 console.log (`Number: ${myNumber}`);
 console.log (`Address: ${myaddress}`);
-
 console.log(67);
+
+function greet(greetings, names = []){
+        if(Array.isArray(names)){
+            names= names.join(',');
+        }
+    return`${greetings} sa enyu, ${names}`;
+}
+console.log(greet("Mayong aga" , ["Arabelle" , "Bai" , "Soy" , "Erp"]));
+
+
        
